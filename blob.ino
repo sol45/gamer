f320edbfc4b74495f71bafcc67df30df55b26f96
Sol Hartel <Sol@Stefanies-MacBook-Air.local>
2016-07-05 17:03:03 +0100

    animate alien and blob

--- blob.ino
+++ blob.ino
@@ -1,40 +1,97 @@
 #include <Gamer.h>
 
 Gamer gamer;
+
 void setup() {
   gamer.begin();
-
 };
 
+struct Alien {
+  int x;
+  int y;
+  boolean movingRight;
+};
 
 struct Blob {
   int x;
   int y;
+  int width;
+  int height;
 };
 
-Blob blob = {4, 4};
+struct Bullet {
+  int x;
+  int y;
+};
 
+Blob blob = {3, 6, 3, 2};
+Bullet bullet = {blob.x+1, 5};
+
+Alien alien = {0, 0, true};
+boolean shot = false;
+
+void drawBlob(int x, int y) {
+  gamer.display[x][y] =1;
+}
+
+void drawAlien(int x, int y) {
+  gamer.display[x][y] =1;
+}
+void animateAlien() {
+  if(alien.x < 7 && alien.movingRight == true) { alien.x++;
+  }
+    else if (alien.x <= 7) { alien.movingRight = false, alien.x--;
+    }
+  if(alien.x == 0 && alien.movingRight== false) {alien.movingRight = true;
+  }  
+}
+
+void drawRect() {
+  for(int i = blob.x; i < (blob.x + blob.width); i++) {
+    for(int j = blob.y; j < (blob.y + blob.height); j++)
+    gamer.display[i][j] = 1;
+    }
+
+}
+ 
+
+void blobControl() {
+  if (gamer.isHeld(RIGHT) && blob.x != 5) {blob.x++;
+  }
+  if (gamer.isHeld(LEFT) && blob.x != 0) {blob.x--;
+   }
+  drawRect();
+  delay(100);
+}
+
+
+//void shoot() {
+// if (gamer.isPressed(UP)) {shot = true;
+// }
+//  while(shot == true) {
+//    
+//    gamer.display[blob.x + 1][y];
+ //   y--;
+ //   }
+///    delay(100);
+//    shot = !shot;
+
+
+//}
+
+void fire(int x, int y) {
+  gamer.display[x][y] = 1;
+}
 
 void loop() {
-wibble(blob.x, blob.y);
-if (gamer.isHeld(RIGHT)) {
-  blob.x++;
-}
-if (gamer.isHeld(LEFT)) {
-  blob.x--;
-}
-if (gamer.isHeld(UP)) {
-  blob.y--;
-}
-if (gamer.isHeld(DOWN)) {
- blob.y++;
-}
-
-}
-void wibble(int x, int y) {
   gamer.clear();
-  gamer.display[x][y] =1;
+  drawRect();
+  drawAlien(alien.x, alien.y);
   gamer.updateDisplay();
-  delay(150);
+  delay(100);
+  animateAlien();
+  blobControl();
+  fire(bullet.x, bullet.y);
+ 
 }
 
