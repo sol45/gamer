f49bcda927001339970c08ef9fff725e41808ba0
Sol Hartel <Sol@Stefanies-MacBook-Air.local>
2016-07-06 17:00:42 +0100

    added multiple aliens in an array

--- blob.ino
+++ blob.ino
@@ -4,9 +4,11 @@ Gamer gamer;
 
 void setup() {
   gamer.begin();
+  Serial.begin(9600);
 };
 
-struct Alien {
+
+ struct Alien{
   int x;
   int y;
   boolean movingRight;
@@ -30,30 +32,30 @@ struct Bullet {
 int score = 0;
 Blob blob = {3, 6, 3, 2};
 Bullet bullet = {5, 6, false};
-Alien alien = {0, 0, true, false};
-
+struct Alien aliens[4];
 
 //void drawBlob(int x, int y) {
 //  gamer.display[x][y] =1;
 //}
 
-void drawAlien(int x, int y) {
-  if(alien.beenShot == false) {gamer.display[x][y] =1;
-}
-}
+//void drawAlien(int x, int y) {
+  //if(alien.beenShot == false) {
+ // gamer.display[x][y] =1;
+ //}
+//}
 void resetBullet(int x, int y) {
   gamer.display[x][y] = 1;
 }
 
 
-void animateAlien() {
-  if(alien.x < 7 && alien.movingRight == true) { alien.x++;
-  }
-    else if (alien.x <= 7) { alien.movingRight = false, alien.x--;
-    }
-  if(alien.x == 0 && alien.movingRight== false) {alien.movingRight = true;
-  }  
-}
+//void animateAlien() {
+//  if(alien.x < 3 && alien.movingRight == true) { alien.x++;
+ // }
+ //   else if (alien.x <= 3) { alien.movingRight = false, alien.x--;
+//    }
+//  if(alien.x == 0 && alien.movingRight== false) {alien.movingRight = true;
+//  }  
+//}
 
 void drawRect() {
   for(int i = blob.x; i < (blob.x + blob.width); i++) {
@@ -62,7 +64,30 @@ void drawRect() {
     }
 
 }
+
+
+void makeAliens() {
+   int x = 0;
+   for(int i = 0; i < 4 ;i++) {
+   aliens[i]= {x, 0, true, false};
+   x=x+2;
  
+   }
+}
+void drawAliens() {
+  for(int i= 0; i < 4; i++) {
+    int x = aliens[i].x;
+    int y = aliens[i].y; 
+   gamer.display[x][y] = 1;
+  }
+}
+
+void moveAliens() {
+  
+}
+
+
+   
 void blobControl() {
   if (gamer.isHeld(RIGHT) && blob.x != 5) {blob.x++;
   }
@@ -85,26 +110,31 @@ void shoot() {
 
 }
 
-void detectImpact() {
-  if(bullet.x == alien.x && bullet.y == alien.y) {
-    score + 10;
-    resetBullet(bullet.x, bullet.y);
-    alien.beenShot = true;
+//void detectImpact() {
+//  if(bullet.x == alien.x && bullet.y == alien.y) {
+ //   score + 10;
+//    resetBullet(bullet.x, bullet.y);
+//    alien.beenShot = true;
     
-    }
-}
+//    }
+//}
 
 void loop() {
   gamer.clear();
   drawRect();
-  drawAlien(alien.x, alien.y);
+  //drawAlien(alien.x, alien.y);
+  makeAliens();
+  drawAliens();
   resetBullet(bullet.x, bullet.y);
   gamer.updateDisplay();
   delay(100);
-  animateAlien();
+//updateAliens();
+  //animateAlien();
   blobControl();
   shoot();
-  detectImpact();
+// detectImpact();
+  
+  
  
 }
 
